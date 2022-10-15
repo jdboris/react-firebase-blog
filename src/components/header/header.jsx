2f77eeeb8fb68f307f6bec19c61b92a187efd7d3
Joe <54695266+jdboris@users.noreply.github.com>
2022-10-15 04:59:16 -0400

    Mobile styles

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,43 +5,41 @@ export function Header({ theme, useFirebaseAuth, useSettings }) {
   const { logo } = useSettings();
 
   return (
-    <header>
-      <div className={theme.container}>
-        <nav>
-          {logo ? (
-            <Link to="/" className={theme.logo}>
-              <img src={logo.url} />
-            </Link>
+    <header className={theme.paddingContainer}>
+      <nav>
+        {logo ? (
+          <Link to="/" className={theme.logo}>
+            <img src={logo.url} />
+          </Link>
+        ) : (
+          <Link to="/">Home</Link>
+        )}
+      </nav>
+      <nav>
+        {!isLoading &&
+          (currentUser ? (
+            <>
+              {currentUser && currentUser.isAuthor && (
+                <>
+                  <Link to="/article/new">New Article</Link>
+                </>
+              )}
+
+              <Link to={`/user/${currentUser.id}`}>Profile</Link>
+
+              {currentUser && currentUser.isAdmin && (
+                <Link to="/settings">Settings</Link>
+              )}
+
+              <button onClick={logout}>Logout</button>
+            </>
           ) : (
-            <Link to="/">Home</Link>
-          )}
-        </nav>
-        <nav>
-          {!isLoading &&
-            (currentUser ? (
-              <>
-                {currentUser && currentUser.isAuthor && (
-                  <>
-                    <Link to="/article/new">New Article</Link>
-                  </>
-                )}
-
-                <Link to={`/user/${currentUser.id}`}>Profile</Link>
-
-                {currentUser && currentUser.isAdmin && (
-                  <Link to="/settings">Settings</Link>
-                )}
-
-                <button onClick={logout}>Logout</button>
-              </>
-            ) : (
-              <>
-                <span onClick={login}>Signup</span>
-                <span onClick={login}>Login</span>
-              </>
-            ))}
-        </nav>
-      </div>
+            <>
+              <span onClick={login}>Signup</span>
+              <span onClick={login}>Login</span>
+            </>
+          ))}
+      </nav>
     </header>
   );
 }
