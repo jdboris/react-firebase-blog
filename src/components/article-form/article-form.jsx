4981a251a1686e03093178b5775625517a11e4b3
Joe <54695266+jdboris@users.noreply.github.com>
2022-10-19 11:48:23 -0400

    Fix bug and tweak styles

--- src/components/article-form/article-form.jsx
+++ src/components/article-form/article-form.jsx
@@ -132,68 +132,67 @@ export function ArticleForm({
               <article>
                 {!isPreview && (
                   <div className={css.tagSection}>
-                    {!isPreview &&
-                      (article?.tags
-                        ? mode === "create" || mode === "edit"
-                          ? [...article?.tags, ""]
-                          : article?.tags
-                        : [""]
-                      )?.map((tag, i) => (
-                        <label key={`tag-${i}`}>
-                          {/* NOTE: Must include a " " */} #
-                          <AutosizeInput
-                            placeholder="tag..."
-                            name={`tag-${i}`}
-                            value={tag}
-                            onChange={(e) => {
-                              setArticle((old) => {
-                                const tags = [
-                                  ...old.tags.slice(0, i),
-                                  e.target.value,
-                                  ...old.tags.slice(i + 1),
-                                ]
-                                  .map((tag) =>
-                                    tag.replace(/\s/g, "").replace(/\W_/g, "")
-                                  )
-                                  .filter((tag) => tag);
+                    {(article?.tags
+                      ? mode === "create" || mode === "edit"
+                        ? [...article?.tags, ""]
+                        : article?.tags
+                      : [""]
+                    )?.map((tag, i) => (
+                      <label key={`tag-${i}`}>
+                        {/* NOTE: Must include a " " */} #
+                        <AutosizeInput
+                          placeholder="tag..."
+                          name={`tag-${i}`}
+                          value={tag}
+                          onChange={(e) => {
+                            setArticle((old) => {
+                              const tags = [
+                                ...old.tags.slice(0, i),
+                                e.target.value,
+                                ...old.tags.slice(i + 1),
+                              ]
+                                .map((tag) =>
+                                  tag.replace(/\s/g, "").replace(/\W_/g, "")
+                                )
+                                .filter((tag) => tag);
 
-                                // // If this is the last tag AND the value ends with a whitespace
-                                // if (
-                                //   (mode === "create" || mode === "edit") &&
-                                //   i === old.tags.length - 1 &&
-                                //   (/\s$/.test(e.target.value) ||
-                                //     e.target.value.trim() === "")
-                                // ) {
-                                //   tags.push("");
-                                // }
+                              // // If this is the last tag AND the value ends with a whitespace
+                              // if (
+                              //   (mode === "create" || mode === "edit") &&
+                              //   i === old.tags.length - 1 &&
+                              //   (/\s$/.test(e.target.value) ||
+                              //     e.target.value.trim() === "")
+                              // ) {
+                              //   tags.push("");
+                              // }
 
-                                return {
-                                  ...old,
-                                  tags,
-                                  lowercaseTags: tags.map((tag) =>
-                                    tag.toLowerCase()
-                                  ),
-                                };
-                              });
-                            }}
-                            onBlur={(e) => {
-                              // const tags = article.tags
-                              //   .map((tag) =>
-                              //     tag.trim().toLowerCase().replace(/\W/g, "")
-                              //   )
-                              //   .filter((tag) => tag);
-                              // // if (mode === "create" || mode === "edit")
-                              // //   tags.push("");
-                              // setArticle((old) => {
-                              //   return {
-                              //     ...old,
-                              //     tags,
-                              //   };
-                              // });
-                            }}
-                          />
-                        </label>
-                      ))}
+                              return {
+                                ...old,
+                                tags,
+                                lowercaseTags: tags.map((tag) =>
+                                  tag.toLowerCase()
+                                ),
+                              };
+                            });
+                          }}
+                          onBlur={(e) => {
+                            // const tags = article.tags
+                            //   .map((tag) =>
+                            //     tag.trim().toLowerCase().replace(/\W/g, "")
+                            //   )
+                            //   .filter((tag) => tag);
+                            // // if (mode === "create" || mode === "edit")
+                            // //   tags.push("");
+                            // setArticle((old) => {
+                            //   return {
+                            //     ...old,
+                            //     tags,
+                            //   };
+                            // });
+                          }}
+                        />
+                      </label>
+                    ))}
                   </div>
                 )}
 
@@ -293,20 +292,7 @@ export function ArticleForm({
             <footer>
               {isPreview && (
                 <h2>
-                  <TextareaAutosize
-                    type="text"
-                    name="title"
-                    value={article?.title}
-                    placeholder="New Article..."
-                    onChange={(e) => {
-                      setArticle((old) => {
-                        return {
-                          ...old,
-                          [e.target.name]: e.target.value,
-                        };
-                      });
-                    }}
-                  />
+                  {article?.title}
 
                   <small>
                     <span>
